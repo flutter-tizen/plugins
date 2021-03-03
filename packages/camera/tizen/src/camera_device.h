242b2897568a64dcd738ec4420ca1aacd6a09b6d
Boram Bae <boram21.bae@samsung.com>
2021-03-03 20:01:49 +0900

    [camera] Establish camera channel and cameraEvents channel
    * Add basics to handle method call of camera channel
    * Add CameraDevice class to represent Camera
    
    Signed-off-by: Boram Bae <boram21.bae@samsung.com>

---
+++ packages/camera/tizen/src/camera_device.h
@@ -0,0 +1,57 @@
+// Copyright 2021 Samsung Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_PLUGIN_CAMERA_DEVICE_H_
+#define FLUTTER_PLUGIN_CAMERA_DEVICE_H_
+
+#include <camera.h>
+#include <flutter/encodable_value.h>
+#include <flutter/method_result.h>
+#include <flutter/plugin_registrar.h>
+#include <flutter_texture_registrar.h>
+
+#include "camera_event_channel.h"
+
+enum class CameraDeviceType {
+  Rear = CAMERA_DEVICE_CAMERA0,  // The back(rear) camera is usually the primary
+  Front = CAMERA_DEVICE_CAMERA1  // the front camera is usually the secondary
+};
+
+struct Size {
+  int width;
+  int height;
+};
+
+class CameraDevice {
+ public:
+  static flutter::EncodableValue GetAvailableCameras();
+
+  CameraDevice();
+  CameraDevice(flutter::PluginRegistrar *registrar,
+               FlutterTextureRegistrar *texture_registrar,
+               CameraDeviceType type);
+  ~CameraDevice();
+
+  void ChangeCameraDeviceType(CameraDeviceType type);
+  void Dispose();
+  Size GetRecommendedPreviewResolution();
+  long GetTextureId() { return texture_id_; }
+
+ private:
+  void CreateCameraHandle();
+  void DestroyCameraHandle();
+
+  int GetDeviceCount();
+  int GetLensOrientation();
+
+  flutter::PluginRegistrar *registrar_{nullptr};
+  FlutterTextureRegistrar *texture_registrar_{nullptr};
+
+  long texture_id_{0};
+  CameraDeviceType type_{CameraDeviceType::Rear};
+  std::unique_ptr<CameraEventChannel> camera_event_channel_;
+  camera_h handle_{nullptr};
+};
+
+#endif
