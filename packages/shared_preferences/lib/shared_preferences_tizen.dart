40ae4b92b00234d43fa1bba6e4378f6681bacfec
Swift Kim <swift.kim@samsung.com>
2021-03-25 20:56:19 +0900

    Update shared_preferences

--- packages/shared_preferences/lib/shared_preferences_tizen.dart
+++ packages/shared_preferences/lib/shared_preferences_tizen.dart
@@ -4,6 +4,7 @@
 
 import 'dart:async';
 import 'dart:ffi';
+
 import 'package:ffi/ffi.dart';
 import 'package:shared_preferences_platform_interface/shared_preferences_platform_interface.dart';
 
@@ -13,59 +14,60 @@ import 'src/bindings.dart';
 ///
 /// This class implements the `package:shared_preferences` functionality for Tizen.
 class SharedPreferencesPlugin extends SharedPreferencesStorePlatform {
+  /// Registers this class as the default instance of [SharedPreferencesStorePlatform].
   static void register() {
     SharedPreferencesStorePlatform.instance = SharedPreferencesPlugin();
   }
 
-  static Map<String, Object> _cachedPreferences;
+  static Map<String, Object>? _cachedPreferences;
   static const String _separator = '␞';
 
   static int _preferenceItemCallback(Pointer<Utf8> pKey, Pointer<Void> data) {
-    final String key = Utf8.fromUtf8(pKey);
+    final String key = pKey.toDartString();
     int ret;
 
-    final Pointer<Int8> pBool = allocate();
+    final Pointer<Int8> pBool = malloc();
     ret = bindings.getBoolean(pKey, pBool);
     final bool boolValue = pBool.value == 1;
-    free(pBool);
+    malloc.free(pBool);
     if (ret == 0) {
-      _cachedPreferences[key] = boolValue;
+      _cachedPreferences![key] = boolValue;
       return 1;
     }
 
-    final Pointer<Double> pDouble = allocate();
+    final Pointer<Double> pDouble = malloc();
     ret = bindings.getDouble(pKey, pDouble);
     final double doubleValue = pDouble.value;
-    free(pDouble);
+    malloc.free(pDouble);
     if (ret == 0) {
-      _cachedPreferences[key] = doubleValue;
+      _cachedPreferences![key] = doubleValue;
       return 1;
     }
 
-    final Pointer<Int32> pInt = allocate();
+    final Pointer<Int32> pInt = malloc();
     ret = bindings.getInt(pKey, pInt);
     final int intValue = pInt.value;
-    free(pInt);
+    malloc.free(pInt);
     if (ret == 0) {
-      _cachedPreferences[key] = intValue;
+      _cachedPreferences![key] = intValue;
       return 1;
     }
 
-    final Pointer<Pointer<Utf8>> ppString = allocate();
+    final Pointer<Pointer<Utf8>> ppString = malloc();
     ret = bindings.getString(pKey, ppString);
     final Pointer<Utf8> pString = ppString.value;
-    free(ppString);
+    malloc.free(ppString);
     if (ret == 0) {
       // The value could be either String or StringList. For details, see
       // setValue().
-      final String stringValue = Utf8.fromUtf8(pString);
+      final String stringValue = pString.toDartString();
       if (stringValue == _separator) {
-        _cachedPreferences[key] = <String>[];
+        _cachedPreferences![key] = <String>[];
       } else if (stringValue.contains(_separator)) {
         final List<String> list = stringValue.split(_separator);
-        _cachedPreferences[key] = list.getRange(1, list.length - 1);
+        _cachedPreferences![key] = list.getRange(1, list.length - 1);
       } else {
-        _cachedPreferences[key] = stringValue;
+        _cachedPreferences![key] = stringValue;
       }
       return 1;
     }
@@ -75,14 +77,14 @@ class SharedPreferencesPlugin extends SharedPreferencesStorePlatform {
 
   Map<String, Object> get _preferences {
     if (_cachedPreferences != null) {
-      return _cachedPreferences;
+      return _cachedPreferences!;
     }
     _cachedPreferences = <String, Object>{};
 
     final int ret = bindings.foreachItem(
         Pointer.fromFunction(_preferenceItemCallback, 0), nullptr);
     if (ret == 0) {
-      return _cachedPreferences;
+      return _cachedPreferences!;
     }
     return <String, Object>{};
   }
@@ -101,14 +103,14 @@ class SharedPreferencesPlugin extends SharedPreferencesStorePlatform {
   Future<bool> remove(String key) async {
     _preferences.remove(key);
 
-    return bindings.remove(Utf8.toUtf8(key)) == 0;
+    return bindings.remove(key.toNativeUtf8()) == 0;
   }
 
   @override
   Future<bool> setValue(String valueType, String key, Object value) async {
     _preferences[key] = value;
 
-    final Pointer<Utf8> pKey = Utf8.toUtf8(key);
+    final Pointer<Utf8> pKey = key.toNativeUtf8();
     int ret;
     if (valueType == 'Bool') {
       ret = bindings.setBoolean(pKey, (value as bool) ? 1 : 0);
@@ -117,7 +119,7 @@ class SharedPreferencesPlugin extends SharedPreferencesStorePlatform {
     } else if (valueType == 'Int') {
       ret = bindings.setInt(pKey, value as int);
     } else if (valueType == 'String') {
-      ret = bindings.setString(pKey, Utf8.toUtf8(value as String));
+      ret = bindings.setString(pKey, (value as String).toNativeUtf8());
     } else if (valueType == 'StringList') {
       // Tizen Preference API doesn't support arrays.
       final List<String> list = value as List<String>;
@@ -127,9 +129,10 @@ class SharedPreferencesPlugin extends SharedPreferencesStorePlatform {
       } else {
         joined = _separator + list.join(_separator) + _separator;
       }
-      ret = bindings.setString(pKey, Utf8.toUtf8(joined));
+      ret = bindings.setString(pKey, joined.toNativeUtf8());
     } else {
       print('Not implemented : valueType[' + valueType + ']');
+      ret = -1;
     }
     return ret == 0;
   }
