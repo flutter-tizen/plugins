4c5a46be4c20e2545b150dca3b1bc6ccc948ffb8
Swift Kim <swift.kim@samsung.com>
2021-04-07 14:54:26 +0900

    Revert "Invoke error callback on launch failure"
    This reverts commit 538918323b7f88f40233d8c8c8203e963dbabeb7.

--- packages/permission_handler/tizen/src/app_settings_manager.cc
+++ packages/permission_handler/tizen/src/app_settings_manager.cc
@@ -22,7 +22,8 @@ AppSettingsManager::AppSettingsManager() {}
 AppSettingsManager::~AppSettingsManager() {}
 
 void AppSettingsManager::OpenAppSettings(OnAppSettingsOpened success_callback,
-                                         OnAppSettingsError error_callback) {
+                                         OnAppSettingsError error_callback)
+{
   app_control_h service = nullptr;
   int result = app_control_create(&service);
   if (result != APP_CONTROL_ERROR_NONE) {
@@ -34,9 +35,10 @@ void AppSettingsManager::OpenAppSettings(OnAppSettingsOpened success_callback,
   result = app_control_send_launch_request(service, NULL, NULL);
   app_control_destroy(service);
   if (result == APP_CONTROL_ERROR_NONE) {
-    LOG_DEBUG("succeeded to open app settings");
+    LOG_DEBUG("successed to open app settings");
     success_callback(true);
   } else {
-    error_callback(ErrorToString(result), "failed to open app settings");
+    LOG_DEBUG("failed to open app settings");
+    success_callback(false);
   }
 }
