22a37744dda2ce052fa0c9d7f288e166e5e6a8ed
Hakkyu Kim <hakkyu.kim@samsung.com>
2021-06-03 11:37:02 +0900

    Create dart plugin class ...
    to avoid setting Linux platform interface
    https://github.com/fluttercommunity/plus_plugins/blob/c7c942d1d19595a536c6ae18168d3781ddef7785/packages/package_info_plus/lib/package_info_plus.dart#L43-L55

---
+++ packages/package_info_plus/lib/package_info_plus_tizen.dart
@@ -0,0 +1,22 @@
+// Copyright 2021 Samsung Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:package_info_plus_platform_interface/package_info_platform_interface.dart';
+
+/// The Tizen implementation of [PackageInfoPlatform].
+///
+/// This class disables platform overriding so that it always uses the default
+/// implementation of [PackageInfoPlatform].
+///
+/// Although [PackageInfo.disablePackageInfoPlatformOverride] is meant to be
+/// used for testing, it is required for Tizen to avoid platform interface
+/// being set to Linux's.
+/// https://github.com/fluttercommunity/plus_plugins/blob/c7c942d1d19595a536c6ae18168d3781ddef7785/packages/package_info_plus/lib/package_info_plus.dart#L43-L55
+class PackageInfoPlugin extends PackageInfoPlatform {
+  static void register() {
+    // ignore: invalid_use_of_visible_for_testing_member
+    PackageInfo.disablePackageInfoPlatformOverride = true;
+  }
+}
