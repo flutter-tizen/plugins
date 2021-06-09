d164a179736db1bcd4f7d399ec1fa51f64ed9c69
Boram Bae <boram21.bae@samsung.com>
2021-06-09 18:30:21 +0900

    [sensors_plus] Copy tests from original
    Signed-off-by: Boram Bae <boram21.bae@samsung.com>

--- packages/sensors_plus/example/integration_test/sensors_plus_test.dart
+++ packages/sensors_plus/example/integration_test/sensors_plus_test.dart
@@ -1,8 +1,8 @@
-// Copyright 2013 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright 2019, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 
-// @dart = 2.9
+// @dart=2.9
 
 import 'dart:async';
 import 'package:flutter_test/flutter_test.dart';
@@ -14,8 +14,7 @@ void main() {
 
   testWidgets('Can subscript to accelerometerEvents and get non-null events',
       (WidgetTester tester) async {
-    final Completer<AccelerometerEvent> completer =
-        Completer<AccelerometerEvent>();
+    final completer = Completer<AccelerometerEvent>();
     StreamSubscription<AccelerometerEvent> subscription;
     subscription = accelerometerEvents.listen((AccelerometerEvent event) {
       completer.complete(event);
