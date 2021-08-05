c47013e4fac38fbd0fcad5d4df2aed8a37afe352
WonYoung Choi <wy80.choi@samsung.com>
2021-08-06 06:11:42 +0900

    [shared_preferences] Fix memory leaks (#166)
    * Fix memory leaks
    
    - Use Arena to release memory allocated in toNativeUtf8() method.
    - Use Arena to release memory allocated with `malloc()`.
    - Free returnted string from the native API.
    
    * Fix styles
    
    * Apply reviews
    
    * cleanup setValue()
    
    * remove unused variable

--- packages/shared_preferences/lib/shared_preferences_tizen.dart
+++ packages/shared_preferences/lib/shared_preferences_tizen.dart
@@ -24,53 +24,42 @@ class SharedPreferencesPlugin extends SharedPreferencesStorePlatform {
 
   static int _preferenceItemCallback(Pointer<Utf8> pKey, Pointer<Void> data) {
     final String key = pKey.toDartString();
-    int ret;
 
-    final Pointer<Int8> pBool = malloc();
-    ret = bindings.getBoolean(pKey, pBool);
-    final bool boolValue = pBool.value == 1;
-    malloc.free(pBool);
-    if (ret == 0) {
-      _cachedPreferences![key] = boolValue;
-      return 1;
-    }
-
-    final Pointer<Double> pDouble = malloc();
-    ret = bindings.getDouble(pKey, pDouble);
-    final double doubleValue = pDouble.value;
-    malloc.free(pDouble);
-    if (ret == 0) {
-      _cachedPreferences![key] = doubleValue;
-      return 1;
-    }
-
-    final Pointer<Int32> pInt = malloc();
-    ret = bindings.getInt(pKey, pInt);
-    final int intValue = pInt.value;
-    malloc.free(pInt);
-    if (ret == 0) {
-      _cachedPreferences![key] = intValue;
-      return 1;
-    }
-
-    final Pointer<Pointer<Utf8>> ppString = malloc();
-    ret = bindings.getString(pKey, ppString);
-    final Pointer<Utf8> pString = ppString.value;
-    malloc.free(ppString);
-    if (ret == 0) {
-      // The value could be either String or StringList. For details, see
-      // setValue().
-      final String stringValue = pString.toDartString();
-      if (stringValue == _separator) {
-        _cachedPreferences![key] = <String>[];
-      } else if (stringValue.contains(_separator)) {
-        final List<String> list = stringValue.split(_separator);
-        _cachedPreferences![key] = list.getRange(1, list.length - 1);
-      } else {
-        _cachedPreferences![key] = stringValue;
+    using((Arena arena) {
+      final Pointer<Int8> pBool = arena();
+      if (bindings.getBoolean(pKey, pBool) == 0) {
+        _cachedPreferences![key] = pBool.value == 1;
+        return;
       }
-      return 1;
-    }
+
+      final Pointer<Double> pDouble = arena();
+      if (bindings.getDouble(pKey, pDouble) == 0) {
+        _cachedPreferences![key] = pDouble.value;
+        return;
+      }
+
+      final Pointer<Int32> pInt = arena();
+      if (bindings.getInt(pKey, pInt) == 0) {
+        _cachedPreferences![key] = pInt.value;
+        return;
+      }
+
+      final Pointer<Pointer<Utf8>> ppString = arena();
+      if (bindings.getString(pKey, ppString) == 0) {
+        final Pointer<Utf8> pString = ppString.value;
+        final String stringValue = pString.toDartString();
+        if (stringValue == _separator) {
+          _cachedPreferences![key] = <String>[];
+        } else if (stringValue.contains(_separator)) {
+          final List<String> list = stringValue.split(_separator);
+          _cachedPreferences![key] = list.getRange(1, list.length - 1);
+        } else {
+          _cachedPreferences![key] = stringValue;
+        }
+        malloc.free(pString);
+        return;
+      }
+    });
 
     return 1;
   }
@@ -101,39 +90,52 @@ class SharedPreferencesPlugin extends SharedPreferencesStorePlatform {
 
   @override
   Future<bool> remove(String key) async {
-    _preferences.remove(key);
-
-    return bindings.remove(key.toNativeUtf8()) == 0;
+    return using((Arena arena) {
+      final bool ret = bindings.remove(key.toNativeUtf8(allocator: arena)) == 0;
+      if (ret) {
+        _preferences.remove(key);
+      }
+      return ret;
+    });
   }
 
   @override
   Future<bool> setValue(String valueType, String key, Object value) async {
-    _preferences[key] = value;
-
-    final Pointer<Utf8> pKey = key.toNativeUtf8();
-    int ret;
-    if (valueType == 'Bool') {
-      ret = bindings.setBoolean(pKey, (value as bool) ? 1 : 0);
-    } else if (valueType == 'Double') {
-      ret = bindings.setDouble(pKey, value as double);
-    } else if (valueType == 'Int') {
-      ret = bindings.setInt(pKey, value as int);
-    } else if (valueType == 'String') {
-      ret = bindings.setString(pKey, (value as String).toNativeUtf8());
-    } else if (valueType == 'StringList') {
-      // Tizen Preference API doesn't support arrays.
-      final List<String> list = value as List<String>;
-      String joined;
-      if (list.isEmpty) {
-        joined = _separator;
-      } else {
-        joined = _separator + list.join(_separator) + _separator;
+    final int ret = using((Arena arena) {
+      final Pointer<Utf8> pKey = key.toNativeUtf8(allocator: arena);
+      switch (valueType) {
+        case 'Bool':
+          return bindings.setBoolean(pKey, (value as bool) ? 1 : 0);
+        case 'Double':
+          return bindings.setDouble(pKey, value as double);
+        case 'Int':
+          return bindings.setInt(pKey, value as int);
+        case 'String':
+          return bindings.setString(
+            pKey,
+            (value as String).toNativeUtf8(allocator: arena),
+          );
+        case 'StringList':
+          return bindings.setString(
+            pKey,
+            _joinStringList(value as List<String>)
+                .toNativeUtf8(allocator: arena),
+          );
+        default:
+          print('Not implemented : valueType[' + valueType + ']');
+          return -1;
       }
-      ret = bindings.setString(pKey, joined.toNativeUtf8());
-    } else {
-      print('Not implemented : valueType[' + valueType + ']');
-      ret = -1;
+    });
+    if (ret == 0) {
+      _preferences[key] = value;
+      return true;
     }
-    return ret == 0;
+    return false;
+  }
+
+  String _joinStringList(List<String> list) {
+    return list.isEmpty
+        ? _separator
+        : _separator + list.join(_separator) + _separator;
   }
 }
