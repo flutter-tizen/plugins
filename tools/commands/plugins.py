27e9827fa3e11e8d0c426e8d1c59e21dd85577e6
Boram Bae <boram21.bae@samsung.com>
2021-08-09 12:37:38 +0900

    Update based on review
    Signed-off-by: Boram Bae <boram21.bae@samsung.com>

--- tools/commands/plugins.py
+++ tools/commands/plugins.py
@@ -6,7 +6,7 @@ import commands.command_utils as command_utils
 
 
 def parse_args(args):
-    parser = command_utils.get_options_parser(run_on_changed_packages=True)
+    parser = command_utils.get_options_parser(run_on_changed_packages=True, base_sha=True)
     return parser.parse_args(args)
 
 
@@ -14,7 +14,7 @@ def run_plugins(argv):
     args = parse_args(argv)
     packages_dir = command_utils.get_package_dir()
     target_plugins, exclude_plugins = command_utils.get_target_plugins(
-        packages_dir, run_on_changed_packages=args.run_on_changed_packages)
+        packages_dir, run_on_changed_packages=args.run_on_changed_packages, base_sha=args.base_sha)
     print(target_plugins)
 
 
