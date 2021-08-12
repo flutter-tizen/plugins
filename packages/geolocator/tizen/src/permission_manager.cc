8c333b21efab5ce4244016962c7360954142316c
Boram Bae <boram21.bae@samsung.com>
2021-08-12 11:42:06 +0900

    [geolocator] Implement onRequestPermission (#174)
    Signed-off-by: Boram Bae <boram21.bae@samsung.com>

--- packages/geolocator/tizen/src/permission_manager.cc
+++ packages/geolocator/tizen/src/permission_manager.cc
@@ -4,19 +4,32 @@
 
 #include "permission_manager.h"
 
+#include <Ecore.h>
 #include <privacy_privilege_manager.h>
+
+#include <vector>
 
 #include "log.h"
 
 namespace {
+
 constexpr char kPrivilegeLocation[] = "http://tizen.org/privilege/location";
+constexpr char kPrivilegeLocationCoarse[] =
+    "http://tizen.org/privilege/location.coarse";
+
+struct PermissionResponse {
+  ppm_call_cause_e cause;
+  ppm_request_result_e result;
+  bool received = false;
+};
+
 }  // namespace
 
 PermissionManager::PermissionManager() {}
 PermissionManager::~PermissionManager() {}
 
 TizenResult PermissionManager::CheckPermissionStatus(
-    PermissionStatus* permission_status) {
+    PermissionStatus *permission_status) {
   PermissionStatus status;
   ppm_check_result_e check_result;
 
@@ -36,4 +49,63 @@ TizenResult PermissionManager::CheckPermissionStatus(
       break;
   }
   return TizenResult();
+}
+
+void PermissionManager::RequestPermssion(const OnSuccess &on_success,
+                                         const OnFailure &on_failure) {
+  std::vector<std::string> permissions = {kPrivilegeLocation,
+                                          kPrivilegeLocationCoarse};
+  std::vector<PermissionStatus> results;
+
+  int ret = 0;
+  for (size_t i = 0; i < permissions.size(); i++) {
+    const char *permission = permissions[i].c_str();
+    PermissionResponse response;
+    ret = ppm_request_permission(
+        permission,
+        [](ppm_call_cause_e cause, ppm_request_result_e result,
+           const char *privilege, void *data) {
+          PermissionResponse *response =
+              static_cast<PermissionResponse *>(data);
+          response->cause = cause;
+          response->result = result;
+          response->received = true;
+        },
+        &response);
+    if (ret != PRIVACY_PRIVILEGE_MANAGER_ERROR_NONE) {
+      LOG_ERROR("Failed to call ppm_request_permission with [%s]", permission);
+      break;
+    }
+    // Wait until ppm_request_permission is done;
+    while (!response.received) {
+      ecore_main_loop_iterate();
+    }
+
+    if (response.cause != PRIVACY_PRIVILEGE_MANAGER_CALL_CAUSE_ANSWER) {
+      LOG_ERROR("permission[%s] request failed with an error", permission);
+      ret = response.cause;
+      break;
+    }
+
+    switch (response.result) {
+      case PRIVACY_PRIVILEGE_MANAGER_REQUEST_RESULT_ALLOW_FOREVER:
+        results.push_back(PermissionStatus::kAlways);
+        break;
+      case PRIVACY_PRIVILEGE_MANAGER_REQUEST_RESULT_DENY_ONCE:
+        results.push_back(PermissionStatus::kDenied);
+        break;
+      case PRIVACY_PRIVILEGE_MANAGER_REQUEST_RESULT_DENY_FOREVER:
+        results.push_back(PermissionStatus::kDeniedForever);
+        break;
+      default:
+        LOG_ERROR("Unknown ppm_request_result_e!");
+        break;
+    }
+  }
+
+  if (ret || results.size() != permissions.size()) {
+    on_failure(TizenResult(ret));
+  } else {
+    on_success(results[0]);
+  }
 }
