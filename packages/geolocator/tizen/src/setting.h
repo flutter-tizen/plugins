5ee3329ac98560a4eec330b8dd447d4fb7fc2950
Boram Bae <boram21.bae@samsung.com>
2021-09-02 13:58:56 +0900

    [geolocator] Implement open App/location settings (#193)
    * [geolocator] Implement open App/location settings
    
    Signed-off-by: Boram Bae <boram21.bae@samsung.com>
    
    * [geolocator] Fix a typo
    
    Signed-off-by: Boram Bae <boram21.bae@samsung.com>

---
+++ packages/geolocator/tizen/src/setting.h
@@ -0,0 +1,17 @@
+// Copyright 2021 Samsung Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SETTING_H_
+#define SETTING_H_
+
+#include "tizen_result.h"
+
+namespace Setting {
+
+TizenResult LaunchAppSetting();
+TizenResult LaunchLocationSetting();
+
+};  // namespace Setting
+
+#endif
