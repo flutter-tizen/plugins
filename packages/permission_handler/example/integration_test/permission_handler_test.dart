d34e1e7068503354c0cb77df0588f74b32c2136c
Boram Bae <boram21.bae@samsung.com>
2021-09-09 10:09:17 +0900

    [permission_handler] Avoid types on closure parameters
    * Remove unused import
    
    Signed-off-by: Boram Bae <boram21.bae@samsung.com>

--- packages/permission_handler/example/integration_test/permission_handler_test.dart
+++ packages/permission_handler/example/integration_test/permission_handler_test.dart
@@ -1,7 +1,5 @@
 // @dart = 2.9
 
-import 'dart:async';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:integration_test/integration_test.dart';
@@ -9,16 +7,16 @@ import 'package:integration_test/integration_test.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('get permission status', (WidgetTester tester) async {
+  testWidgets('get permission status', (tester) async {
     expect(await Permission.camera.status.isDenied, true);
   });
 
-  testWidgets('get location service status', (WidgetTester tester) async {
+  testWidgets('get location service status', (tester) async {
     var status = await Permission.location.serviceStatus;
     expect(status.isEnabled || status.isDisabled, true);
   });
 
-  testWidgets('open app settings', (WidgetTester tester) async {
+  testWidgets('open app settings', (tester) async {
     expect(await openAppSettings(), true);
   }, skip: true);
 }
