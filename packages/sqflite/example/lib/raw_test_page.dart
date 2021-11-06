b84a811af083f973f1560a244ca884a253197870
Renzo Manganiello <renzo@fmanganiello.com.ar>
2021-11-06 11:53:24 -0300

    Fix no-rowid tests for tizen

--- packages/sqflite/example/lib/raw_test_page.dart
+++ packages/sqflite/example/lib/raw_test_page.dart
@@ -505,8 +505,9 @@ class RawTestPage extends TestPage {
     });
 
     test('without rowid', () async {
-      // Sqflite.devSetDebugModeOn(true);
-      // this fails on iOS
+      // Inserts into WITHOUT ROWID tables are not recorded. If no successful INSERTs
+      // into rowid tables have ever occurred on the database connection, then returns zero.
+      // Ref: https://www.sqlite.org/c3ref/last_insert_rowid.html
 
       late Database db;
       try {
@@ -517,19 +518,9 @@ class RawTestPage extends TestPage {
         await db
             .execute('CREATE TABLE Test (name TEXT PRIMARY KEY) WITHOUT ROWID');
         var id = await db.insert('Test', {'name': 'test'});
-        // it seems to always return 1 on Android, 0 on iOS...
-        if (Platform.isIOS || Platform.isMacOS) {
-          expect(id, 0);
-        } else {
-          expect(id, 1);
-        }
+        expect(id, 0);
         id = await db.insert('Test', {'name': 'other'});
-        // it seems to always return 1
-        if (Platform.isIOS || Platform.isMacOS) {
-          expect(id, 0);
-        } else {
-          expect(id, 1);
-        }
+        expect(id, 0);
         // notice the order is based on the primary key
         final list = await db.query('Test');
         expect(list, [
