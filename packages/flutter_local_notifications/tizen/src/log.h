3027445bfe617fde6cb3e0df8ea34134b79827bb
rojin <rojin93.jung@samsung.com>
2021-12-16 18:09:10 +0900

    Add default code

---
+++ packages/flutter_local_notifications/tizen/src/log.h
@@ -0,0 +1,24 @@
+#ifndef __LOG_H__
+#define __LOG_H__
+
+#include <dlog.h>
+
+#ifdef LOG_TAG
+#undef LOG_TAG
+#endif
+#define LOG_TAG "FlutterLocalNotificationsPlugin"
+
+#ifndef __MODULE__
+#define __MODULE__ strrchr("/" __FILE__, '/') + 1
+#endif
+
+#define LOG(prio, fmt, arg...)                                         \
+  dlog_print(prio, LOG_TAG, "%s: %s(%d) > " fmt, __MODULE__, __func__, \
+             __LINE__, ##arg)
+
+#define LOG_DEBUG(fmt, args...) LOG(DLOG_DEBUG, fmt, ##args)
+#define LOG_INFO(fmt, args...) LOG(DLOG_INFO, fmt, ##args)
+#define LOG_WARN(fmt, args...) LOG(DLOG_WARN, fmt, ##args)
+#define LOG_ERROR(fmt, args...) LOG(DLOG_ERROR, fmt, ##args)
+
+#endif  // __LOG_H__
