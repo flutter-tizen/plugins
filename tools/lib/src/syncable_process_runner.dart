db06ce8ac3b8ac68609cf1d2313ee8beb5d4a6fe
Hakkyu Kim <hakkyu271@gmail.com>
2022-02-16 17:06:19 +0900

    Add TizenSdk class

---
+++ tools/lib/src/syncable_process_runner.dart
@@ -0,0 +1,64 @@
+// Copyright 2022 Samsung Electronics Co., Ltd. All rights reserved.
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:convert';
+import 'dart:io' as io;
+
+import 'package:file/file.dart';
+import 'package:flutter_plugin_tools/src/common/core.dart';
+import 'package:flutter_plugin_tools/src/common/process_runner.dart';
+
+/// A class used to run processes synchronously.
+///
+/// We extend [ProcessRunner] to stub behaviors in tests.
+class SyncableProcessRunner extends ProcessRunner {
+  /// Creates a new syncable process runner.
+  const SyncableProcessRunner();
+
+  /// Run the [executable] with [args].
+  ///
+  /// This is synchronous version of [run].
+  ///
+  /// The current working directory of [executable] can be overridden by
+  /// passing [workingDir].
+  ///
+  /// If [exitOnError] is set to `true`, then this will throw an error if
+  /// the [executable] terminates with a non-zero exit code.
+  /// Defaults to `false`.
+  ///
+  /// If [logOnError] is set to `true`, it will print a formatted message about the error.
+  /// Defaults to `false`
+  ///
+  /// Returns the [io.ProcessResult] of the [executable].
+  io.ProcessResult runSync(String executable, List<String> args,
+      {Directory? workingDir,
+      bool exitOnError = false,
+      bool logOnError = false,
+      Encoding stdoutEncoding = io.systemEncoding,
+      Encoding stderrEncoding = io.systemEncoding}) {
+    final io.ProcessResult result = io.Process.runSync(executable, args,
+        workingDirectory: workingDir?.path,
+        stdoutEncoding: stdoutEncoding,
+        stderrEncoding: stderrEncoding);
+    if (result.exitCode != 0) {
+      if (logOnError) {
+        final String error =
+            _getErrorString(executable, args, workingDir: workingDir);
+        print('$error Stderr:\n${result.stdout}');
+      }
+      if (exitOnError) {
+        throw ToolExit(result.exitCode);
+      }
+    }
+    return result;
+  }
+
+  /// Copied from [ProcessRunner._getErrorString].
+  String _getErrorString(String executable, List<String> args,
+      {Directory? workingDir}) {
+    final String workdir = workingDir == null ? '' : ' in ${workingDir.path}';
+    return 'ERROR: Unable to execute "$executable ${args.join(' ')}"$workdir.';
+  }
+}
