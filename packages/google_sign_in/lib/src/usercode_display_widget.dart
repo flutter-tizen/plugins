67e5e5464c1054d42fbc99021de84608bbe700c1
Hakkyu Kim <hakkyu271@gmail.com>
2022-07-19 17:19:22 +0900

    Verify deviceflow widget is on top before popping

--- packages/google_sign_in/lib/src/usercode_display_widget.dart
+++ packages/google_sign_in/lib/src/usercode_display_widget.dart
@@ -4,9 +4,16 @@
 
 import 'package:flutter/material.dart';
 
+class _DeviceFlowWidgetKey extends GlobalObjectKey {
+  const _DeviceFlowWidgetKey(super.value);
+}
+
 /// Closes the Dialog that was shown from [displayUserCodeDialog].
 void closeUserCodeDialog(GlobalKey<NavigatorState> navigatorKey) {
-  Navigator.pop(navigatorKey.currentContext!);
+  if (navigatorKey.currentWidget?.key ==
+      const _DeviceFlowWidgetKey('device-flow')) {
+    Navigator.pop(navigatorKey.currentContext!);
+  }
 }
 
 /// Displays a Dialog that shows [userCode] and [verificationUrl].
@@ -21,6 +28,7 @@ void displayUserCodeDialog(
     barrierDismissible: false,
     builder: (BuildContext context) {
       return AlertDialog(
+        key: const _DeviceFlowWidgetKey('device-flow'),
         title: const Text('Google SignIn'),
         content: Column(
           mainAxisAlignment: MainAxisAlignment.center,
