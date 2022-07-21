31f635bfbffa0f3e10773249ccb9f1697bfcee9c
Hakkyu Kim <hakkyu271@gmail.com>
2022-07-21 11:45:34 +0900

    Add dummy dart credential file to pass CI

---
+++ packages/google_sign_in/example/lib/fake_credentials.dart
@@ -0,0 +1,4 @@
+// ignore_for_file: public_member_api_docs
+
+const String cliendId = 'clientId';
+const String clientSecret = 'clientSecret';
--- packages/google_sign_in/example/lib/main.dart
+++ packages/google_sign_in/example/lib/main.dart
@@ -12,7 +12,10 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:google_sign_in_tizen/google_sign_in_tizen.dart' as tizen;
 import 'package:http/http.dart' as http;
 
-import 'credentials.dart' as credentials;
+// `credentials.dart` contains the actual "client id" and "client secret".
+// `fake_credentials.dart` is a dummy file to pass CI.
+import 'fake_credentials.dart' as credentials;
+// import 'credentials.dart' as credentials;
 
 GoogleSignIn _googleSignIn = GoogleSignIn(
   scopes: <String>[
