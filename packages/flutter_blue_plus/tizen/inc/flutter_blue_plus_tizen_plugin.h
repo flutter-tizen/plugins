037c79e93eee583ea654247cf888c7ba5d96640c
JRazek <jakub.razek2@gmail.com>
2022-08-30 13:58:30 +0200

    style fixes

--- packages/flutter_blue_plus/tizen/inc/flutter_blue_plus_tizen_plugin.h
+++ packages/flutter_blue_plus/tizen/inc/flutter_blue_plus_tizen_plugin.h
@@ -1,3 +1,7 @@
+// Copyright 2022 Samsung Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef FLUTTER_BLUE_TIZEN__PLUGIN_H_
 #define FLUTTER_BLUE_TIZEN__PLUGIN_H_
 
