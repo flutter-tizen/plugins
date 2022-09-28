90e0662c4b0cd63cae2126b7db0e42bab32fff38
Swift Kim <swift.kim@samsung.com>
2022-09-28 16:06:57 +0900

    Simplify example app and integration_test

--- packages/tizen_bundle/example/lib/main.dart
+++ packages/tizen_bundle/example/lib/main.dart
@@ -1,14 +1,17 @@
+// Copyright 2022 Samsung Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
 import 'package:flutter/material.dart';
 import 'package:tizen_bundle/tizen_bundle.dart';
 
-/// The main entry point.
 void main() {
   runApp(const MyApp());
 }
 
-/// The UI app widget.
 class MyApp extends StatefulWidget {
-  /// The UI app widget.
   const MyApp({super.key});
 
   @override
@@ -18,12 +21,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final Bundle _bundle = Bundle();
   int _count = 0;
-  String _msg = '';
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,74 +33,30 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const Text(
-                'Bundle example',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 50.0),
-                child: Text('Message: $_msg\n'),
-              ),
-              const SizedBox(height: 20),
-              TextButton(
+              Text('Current size: ${_bundle.length}'),
+              const SizedBox(height: 10),
+              ElevatedButton(
                 onPressed: () {
-                  try {
-                    _bundle['stringKey_${_count++}'] = 'stringValue_$_count';
-                    setState(() {
-                      _msg = 'addString done.';
-                    });
-                  } on Exception catch (_, e) {
-                    setState(() {
-                      _msg = 'Exception($e) occurs';
-                    });
-                  }
+                  _bundle['stringKey_${_count++}'] = 'stringValue_$_count';
+                  setState(() {});
                 },
                 child: const Text('Add String'),
               ),
-              TextButton(
+              const SizedBox(height: 10),
+              ElevatedButton(
                 onPressed: () {
-                  if (_count > 0) {
-                    final Object? value = _bundle['stringKey_${_count - 1}'];
-                    setState(() {
-                      _msg = 'getString done. value: $value';
-                    });
-                  } else {
-                    setState(() {
-                      _msg = 'Bundle is empty';
-                    });
-                  }
-                },
-                child: const Text('Get String'),
-              ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    _msg = 'length: ${_bundle.length}';
-                  });
-                },
-                child: const Text('Get length'),
-              ),
-              TextButton(
-                onPressed: () {
-                  if (_count > 0) {
-                    _bundle.remove('stringKey_${--_count}');
-                    setState(() {
-                      _msg = 'remove done.';
-                    });
-                  } else {
-                    setState(() {
-                      _msg = 'Bundle is empty';
-                    });
+                  if (_bundle.isNotEmpty) {
+                    _bundle.remove(_bundle.keys.last);
+                    setState(() {});
                   }
                 },
                 child: const Text('Remove String'),
               ),
-              TextButton(
+              const SizedBox(height: 10),
+              ElevatedButton(
                 onPressed: () {
                   _bundle.clear();
-                  setState(() {
-                    _msg = 'clear done.';
-                  });
+                  setState(() {});
                 },
                 child: const Text('Clear'),
               ),
