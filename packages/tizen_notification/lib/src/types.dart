5a898a3cdd26d8f216e658a1c8b27c88f93ff864
Swift Kim <swift.kim@samsung.com>
2022-11-21 11:43:20 +0900

    [tizen_notification] Bump version to 0.2.0 (#481)

--- packages/tizen_notification/lib/src/types.dart
+++ packages/tizen_notification/lib/src/types.dart
@@ -2,102 +2,199 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// ignore_for_file: public_member_api_docs
+import 'package:tizen_app_control/tizen_app_control.dart';
 
-import 'enums.dart';
+export 'package:tizen_app_control/tizen_app_control.dart';
 
-/// Properties that configure how the system handles notification at
-/// certain scenarios.
-class Property {
-  /// Display only SIM card inserted.
-  static const int displayOnlySimMode = 1 << 0;
+/// How the system handles the notification in certain scenarios.
+class NotificationProperty {
+  // The constants below are originally defined in notification_type.h as
+  // the enum type _notification_property.
+  NotificationProperty._();
 
-  /// Disable application launch when notification is selected.
-  static const int disableAppLaunch = 1 << 1;
+  /// Display only if a SIM card is inserted.
+  static const int onlySimMode = 0x00000001;
 
-  /// Disable auto delete when notification is selected.
-  static const int disableAutoDelete = 1 << 2;
+  /// Do not perform any operation when the notification is clicked.
+  static const int disableAppLaunch = 0x00000002;
 
-  /// Delete notification when device is rebooted.
-  static const int volatileDisplay = 1 << 8;
+  /// Do not dismiss the notification when clicked.
+  static const int disableAutoDelete = 0x00000004;
+
+  /// Dismiss the notification when the device is rebooted.
+  static const int volatile = 0x00000100;
 }
 
-/// The destination app that displays notification.
-class DisplayApplist {
-  /// Notification Tray(Quickpanel).
-  static const int tray = 1 << 0;
+/// Where and how the notification should be presented.
+class NotificationStyle {
+  // The constants below are originally defined in notification_type.h as
+  // the enum type _notification_display_applist.
+  NotificationStyle._();
 
-  /// Ticker notification.
-  static const int ticker = 1 << 1;
+  /// Display in the notification area of the quick panel.
+  static const int tray = 0x00000001;
 
-  /// Lock screen.
-  static const int lock = 1 << 2;
+  /// Display in the lock screen.
+  static const int lock = 0x00000004;
 
-  /// Indicator.
-  static const int indicator = 1 << 3;
+  /// Display in the indicator area (the top of the screen).
+  static const int indicator = 0x00000002 | 0x00000008;
 
-  /// All display application.
-  static const int all = (1 << 4) - 1;
+  /// All of the above.
+  static const int all = tray | lock | indicator;
 }
 
-class NotificationImage {
-  /// [NotificationImage] specifies image options for notifications.
-  NotificationImage({
-    this.iconPath,
-    this.indicatorPath,
-    this.lockPath,
+/// A set of icons to be shown in the notification layouts.
+class NotificationIcons {
+  /// Creates a [NotificationIcons] with the given icon paths.
+  NotificationIcons({
+    this.icon,
+    this.indicatorIcon,
+    this.lockIcon,
   });
 
-  /// The path of icon.
-  final String? iconPath;
+  /// The path to the icon file.
+  String? icon;
 
-  /// The path of indicator icon.
-  final String? indicatorPath;
+  /// The path to the indicator icon file.
+  String? indicatorIcon;
 
-  /// The path of lock screen icon.
-  final String? lockPath;
+  /// The path to the lock screen icon file.
+  String? lockIcon;
 
-  /// Returns [NotificationImage] member fields in a map format.
-  Map<String, dynamic> toMap() => <String, dynamic>{
-        'icon': iconPath,
-        'iconForIndicator': indicatorPath,
-        'iconForLock': lockPath,
+  /// Converts to a map.
+  Map<String, String?> toMap() => <String, String?>{
+        'icon': icon,
+        'iconForIndicator': indicatorIcon,
+        'iconForLock': lockIcon,
       };
 }
 
+/// The type of sound.
+enum SoundType {
+  /// No sound.
+  none,
+
+  /// Default sound.
+  builtIn,
+
+  /// User sound data.
+  userData,
+}
+
+/// The sound to play when the notification is presented.
 class NotificationSound {
-  /// [NotificationSound] specifies sound options for notifications.
-  ///
-  /// [path] refers to a file path to custom sound data played on notification,
-  /// this value is ignored When [type] is not [SoundType.userData].
-  NotificationSound({
-    required this.type,
-    this.path,
-  });
+  /// Creates a [NotificationSound] with the given [type] and [path].
+  NotificationSound({required this.type, this.path});
 
-  final SoundType type;
+  /// The type of sound.
+  SoundType type;
+
+  /// The path to the user sound data file.
+  ///
+  /// Only applicable if the [type] is [SoundType.userData].
   String? path;
 
-  /// Returns [NotificationSound] member fields in a map format.
-  Map<String, dynamic> toMap() => <String, dynamic>{
+  /// Converts to a map.
+  Map<String, String?> toMap() => <String, String?>{
         'type': type.name,
         'path': path,
       };
 }
 
-class NotificationVibration {
-  /// [NotificationVibration] specifies vibration options for notifications.
-  NotificationVibration({
-    required this.type,
-    this.path,
-  });
+/// The type of vibration.
+enum VibrationType {
+  /// No vibration.
+  none,
 
-  final VibrationType type;
+  /// Default vibration pattern.
+  builtIn,
+
+  /// User vibration data.
+  userData,
+}
+
+/// The notification vibration options.
+class NotificationVibration {
+  /// Creates a [NotificationVibration] with the given [type] and [path].
+  NotificationVibration({required this.type, this.path});
+
+  /// The type of vibration.
+  VibrationType type;
+
+  /// The path to the user vibration data file.
+  ///
+  /// Only applicable if the [type] is [VibrationType.userData].
   String? path;
 
-  /// Returns [NotificationVibration] member fields in a map format.
-  Map<String, dynamic> toMap() => <String, dynamic>{
+  /// Converts to a map.
+  Map<String, String?> toMap() => <String, String?>{
         'type': type.name,
         'path': path,
+      };
+}
+
+extension _AppControlToMap on AppControl {
+  /// Converts to a map.
+  Map<String, Object?> toMap() => <String, Object?>{
+        'appId': appId,
+        'operation': operation,
+        'uri': uri,
+        'mime': mime,
+        'extraData': extraData,
+      };
+}
+
+/// The notification details.
+class TizenNotificationDetails {
+  /// Constructs a [TizenNotificationDetails] from the given properties.
+  TizenNotificationDetails({
+    this.icons,
+    this.sound,
+    this.vibration,
+    this.properties = 0,
+    this.style = NotificationStyle.all,
+    this.ongoing = false,
+    this.appControl,
+  });
+
+  /// A set of icons to be shown in the notification layouts.
+  NotificationIcons? icons;
+
+  /// The sound to play when the notification is presented.
+  NotificationSound? sound;
+
+  /// The notification vibration options.
+  NotificationVibration? vibration;
+
+  /// Specifies how the system handles the notification in certain scenarios.
+  ///
+  /// Multiple [NotificationProperty] values can be set using the bitwise OR
+  /// operator (|).
+  int properties;
+
+  /// Specifies where and how the notification should be presented.
+  ///
+  /// Multiple [NotificationStyle] values can be set using the bitwise OR
+  /// operator (|).
+  int style;
+
+  /// Whether the notification can be dismissed by user.
+  ///
+  /// Only supported on Raspberry Pi (common profile) devices.
+  bool ongoing;
+
+  /// A control message to be sent when the notification is clicked.
+  AppControl? appControl;
+
+  /// Converts to a map.
+  Map<String, Object?> toMap() => <String, Object?>{
+        'image': icons?.toMap(),
+        'sound': sound?.toMap(),
+        'vibration': vibration?.toMap(),
+        'properties': properties,
+        'displayApplist': style,
+        'ongoing': ongoing,
+        'appControl': appControl?.toMap(),
       };
 }
