3851375c01705ce62b507ec15eb0fad7a8321bcd
JunsuChoi <jsuya.choi@samsung.com>
2023-01-18 17:05:11 +0900

    Fix build error

--- packages/tizen_audio_manager/CHANGELOG.md
+++ packages/tizen_audio_manager/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1
+
+* Code refactoring.
+
 ## 0.1.0
 
 * Initial release.
---
+++ packages/tizen_audio_manager/tizen/src/tizen_audio_manager.h
@@ -0,0 +1,43 @@
+// Copyright 2023 Samsung Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_PLUGIN_TIZEN_AUDIO_MANAGER_H_
+#define FLUTTER_PLUGIN_TIZEN_AUDIO_MANAGER_H_
+
+#include <sound_manager.h>
+
+#include <functional>
+#include <string>
+
+using OnVolumeChanged =
+    std::function<void(const std::string& type_str, int32_t volume)>;
+
+class TizenAudioManager {
+ public:
+  TizenAudioManager() {}
+  ~TizenAudioManager() {}
+
+  static TizenAudioManager& GetInstance() {
+    static TizenAudioManager instance;
+    return instance;
+  }
+
+  std::string getCurrentPlaybackType();
+
+  int32_t GetMaxVolume(const std::string& type);
+
+  void SetVolume(const std::string& type, int32_t volume);
+
+  int32_t GetVolume(const std::string& type);
+
+  void RegisterVolumeChangedCallback(OnVolumeChanged volume_changed_callback);
+
+  void UnregisterVolumeChangedCallback();
+
+ private:
+  int32_t volume_changed_cb_id_ = 0;
+  OnVolumeChanged volume_changed_callback_ = nullptr;
+};
+
+#endif  // FLUTTER_PLUGIN_TIZEN_AUDIO_MANAGER_H_
