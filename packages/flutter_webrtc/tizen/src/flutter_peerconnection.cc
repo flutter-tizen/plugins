b6b0c17c1c2d1b07ef9a9d51bf57d2302e33f1ae
wanchao-xu <wanchao.xu@samsung.com>
2023-05-09 22:21:53 +0800

    Fix the crash issue of video room.

--- packages/flutter_webrtc/tizen/src/flutter_peerconnection.cc
+++ packages/flutter_webrtc/tizen/src/flutter_peerconnection.cc
@@ -276,6 +276,12 @@ void FlutterPeerConnection::CreateRTCPeerConnection(
 void FlutterPeerConnection::RTCPeerConnectionClose(
     RTCPeerConnection* pc, const std::string& uuid,
     std::unique_ptr<MethodResultProxy> result) {
+  auto it = base_->peerconnection_observers_.find(uuid);
+  if (it != base_->peerconnection_observers_.end())
+    base_->peerconnection_observers_.erase(it);
+  auto it2 = base_->peerconnections_.find(uuid);
+  if (it2 != base_->peerconnections_.end()) base_->peerconnections_.erase(it2);
+
   pc->Close();
   result->Success();
 }
@@ -283,11 +289,6 @@ void FlutterPeerConnection::RTCPeerConnectionClose(
 void FlutterPeerConnection::RTCPeerConnectionDispose(
     RTCPeerConnection* pc, const std::string& uuid,
     std::unique_ptr<MethodResultProxy> result) {
-  auto it = base_->peerconnection_observers_.find(uuid);
-  if (it != base_->peerconnection_observers_.end())
-    base_->peerconnection_observers_.erase(it);
-  auto it2 = base_->peerconnections_.find(uuid);
-  if (it2 != base_->peerconnections_.end()) base_->peerconnections_.erase(it2);
   result->Success();
 }
 
