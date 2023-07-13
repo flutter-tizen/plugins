2bd185d9dd0af9ded237fe86c6a258371f144996
yue7.huang <yue7.huang@samsung.com>
2023-07-13 18:04:38 +0800

    Set biiling api as public function

--- packages/in_app_purchase/tizen/src/billing_service_proxy.h
+++ packages/in_app_purchase/tizen/src/billing_service_proxy.h
@@ -29,17 +29,18 @@ typedef bool (*FuncGetpurchaselist)(const char *app_id, const char *custom_id,
                                     billing_server_type server_type,
                                     billing_payment_api_cb callback,
                                     void *user_data);
-typedef bool (*FuncBillingBuyItem)(const char *app_id, const char *server_type,
-                                   const char *detail_info);
-typedef void (*FuncBillingSetBuyItemCb)(billing_buyitem_cb callback,
-                                        void *user_data);
-typedef bool (*FuncServiceBillingIsServiceAvailable)(
-    billing_server_type server_type, billing_payment_api_cb callback,
-    void *user_data);
-typedef bool (*FuncServiceBillingVerifyInvoice)(
-    const char *app_id, const char *custom_id, const char *invoice_id,
-    const char *country_code, billing_server_type server_type,
-    billing_payment_api_cb callback, void *user_data);
+typedef bool (*FuncBuyItem)(const char *app_id, const char *server_type,
+                            const char *detail_info);
+typedef void (*FuncSetBuyItemCb)(billing_buyitem_cb callback, void *user_data);
+typedef bool (*FuncIsServiceAvailable)(billing_server_type server_type,
+                                       billing_payment_api_cb callback,
+                                       void *user_data);
+typedef bool (*FuncVerifyInvoice)(const char *app_id, const char *custom_id,
+                                  const char *invoice_id,
+                                  const char *country_code,
+                                  billing_server_type server_type,
+                                  billing_payment_api_cb callback,
+                                  void *user_data);
 
 class BillingWrapper {
  public:
@@ -54,16 +55,38 @@ class BillingWrapper {
   BillingWrapper &operator=(const BillingWrapper &) = delete;
 
   bool Initialize();
-  FuncGetProductslist service_billing_get_products_list = nullptr;
-  FuncGetpurchaselist service_billing_get_purchase_list = nullptr;
-  FuncBillingBuyItem service_billing_buyitem = nullptr;
-  FuncBillingSetBuyItemCb service_billing_set_buyitem_cb = nullptr;
-  FuncServiceBillingIsServiceAvailable service_billing_is_service_available =
-      nullptr;
-  FuncServiceBillingVerifyInvoice service_billing_verify_invoice = nullptr;
+
+  bool service_billing_get_products_list(
+      const char *app_id, const char *country_code, int page_size,
+      int page_number, const char *check_value, billing_server_type server_type,
+      billing_payment_api_cb callback, void *user_data);
+  bool service_billing_get_purchase_list(
+      const char *app_id, const char *custom_id, const char *country_code,
+      int page_number, const char *check_value, billing_server_type server_type,
+      billing_payment_api_cb callback, void *user_data);
+  bool service_billing_buyitem(const char *app_id, const char *server_type,
+                               const char *detail_info);
+  void service_billing_set_buyitem_cb(billing_buyitem_cb callback,
+                                      void *user_data);
+  bool service_billing_is_service_available(billing_server_type server_type,
+                                            billing_payment_api_cb callback,
+                                            void *user_data);
+  bool service_billing_verify_invoice(const char *app_id, const char *custom_id,
+                                      const char *invoice_id,
+                                      const char *country_code,
+                                      billing_server_type server_type,
+                                      billing_payment_api_cb callback,
+                                      void *user_data);
 
  private:
   BillingWrapper();
+
+  FuncGetProductslist get_products_list = nullptr;
+  FuncGetpurchaselist get_purchase_list = nullptr;
+  FuncBuyItem buyitem = nullptr;
+  FuncSetBuyItemCb set_buyitem_cb = nullptr;
+  FuncIsServiceAvailable is_service_available = nullptr;
+  FuncVerifyInvoice verify_invoice = nullptr;
 
   void *handle_ = nullptr;
 };
