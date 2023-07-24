1ab6b9987e2fe7253fc9278d9daceb598e81eed1
Mehmet Ali <ali.ustaoglu@icloud.com>
2023-07-24 09:02:18 +0200

    [video_player_videohole] fix web build

---
+++ packages/video_player_videohole/lib/src/register_drm_callback_noop.dart
@@ -0,0 +1,27 @@
+import 'package:video_player_videohole/src/drm_configs.dart';
+
+/// Register DRM callback function (No-op implementation).
+///
+/// This function is a no-op (no-operation) implementation for registering the DRM callback
+/// and communication channel to handle license challenges for video playback. It does not
+/// perform any actual DRM operations but serves as a placeholder to maintain consistency
+/// in the codebase when DRM functionality is not required.
+///
+/// The [drmConfigs] parameter contains configuration details for DRM operations, if any.
+/// If DRM capabilities are not needed, this parameter can be set to null.
+///
+/// The [playerId] parameter represents the unique identifier of the video player.
+///
+/// This function does not perform any communication with the native side or any DRM-related
+/// operations. It can be used when DRM functionality is not required, or when a specific DRM
+/// plugin or platform support is not available.
+///
+/// Example usage:
+/// ```dart
+/// registerDrmCallback(drmConfigs, _playerId);
+/// ```
+///
+/// Note: This function does not have any DRM functionality and should only be used in cases
+/// where DRM support is not necessary or when using a DRM plugin that does not require native
+/// communication for license handling.
+void registerDrmCallback(DrmConfigs? drmConfigs, int playerId) {}
