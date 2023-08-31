05918a7afbe037af19ce49f49d8399a6e896f6a7
Swift Kim <swift.kim@samsung.com>
2023-08-31 14:57:28 +0900

    Resolve Flutter 3.13 analyze issues (#612)

--- packages/audioplayers/example/lib/tabs/audio_context.dart
+++ packages/audioplayers/example/lib/tabs/audio_context.dart
@@ -151,7 +151,7 @@ class AudioContextTabState extends State<AudioContextTab>
         LabeledDropDown<AndroidContentType>(
           label: 'contentType',
           key: const Key('contentType'),
-          options: {for (var e in AndroidContentType.values) e: e.name},
+          options: {for (final e in AndroidContentType.values) e: e.name},
           selected: audioContext.android.contentType,
           onChange: (v) => updateAudioContextAndroid(
             audioContext.android.copy(contentType: v),
@@ -160,7 +160,7 @@ class AudioContextTabState extends State<AudioContextTab>
         LabeledDropDown<AndroidUsageType>(
           label: 'usageType',
           key: const Key('usageType'),
-          options: {for (var e in AndroidUsageType.values) e: e.name},
+          options: {for (final e in AndroidUsageType.values) e: e.name},
           selected: audioContext.android.usageType,
           onChange: (v) => updateAudioContextAndroid(
             audioContext.android.copy(usageType: v),
@@ -169,7 +169,7 @@ class AudioContextTabState extends State<AudioContextTab>
         LabeledDropDown<AndroidAudioFocus?>(
           key: const Key('audioFocus'),
           label: 'audioFocus',
-          options: {for (var e in AndroidAudioFocus.values) e: e.name},
+          options: {for (final e in AndroidAudioFocus.values) e: e.name},
           selected: audioContext.android.audioFocus,
           onChange: (v) => updateAudioContextAndroid(
             audioContext.android.copy(audioFocus: v),
@@ -178,7 +178,7 @@ class AudioContextTabState extends State<AudioContextTab>
         LabeledDropDown<AndroidAudioMode>(
           key: const Key('audioMode'),
           label: 'audioMode',
-          options: {for (var e in AndroidAudioMode.values) e: e.name},
+          options: {for (final e in AndroidAudioMode.values) e: e.name},
           selected: audioContext.android.audioMode,
           onChange: (v) => updateAudioContextAndroid(
             audioContext.android.copy(audioMode: v),
@@ -213,13 +213,13 @@ class AudioContextTabState extends State<AudioContextTab>
         LabeledDropDown<AVAudioSessionCategory>(
           key: const Key('category'),
           label: 'category',
-          options: {for (var e in AVAudioSessionCategory.values) e: e.name},
+          options: {for (final e in AVAudioSessionCategory.values) e: e.name},
           selected: audioContext.iOS.category,
           onChange: (v) => updateAudioContextIOS(
             audioContext.iOS.copy(category: v),
           ),
         ),
-        ...iosOptions
+        ...iosOptions,
       ],
     );
   }
