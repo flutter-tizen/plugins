d4bd30e065edd7861e9e54fce10be77e39c92070
Swift Kim <swift.kim@samsung.com>
2023-10-25 11:17:18 +0900

    [sqflite] Downgrade flutter_lints to resolve analyze issues

--- packages/sqflite/lib/sqflite_tizen.dart
+++ packages/sqflite/lib/sqflite_tizen.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore_for_file: avoid_classes_with_only_static_members
+
 import 'package:sqflite/sqflite.dart';
 
 /// A class to initialize the plugin.
