64439f7a2bee06b6fa776660a476eb111720c9d9
Xiaowei Guan <xiaowei.guan@samsung.com>
2025-03-05 15:43:03 +0800

    Format

--- packages/video_player_avplay/tizen/inc/rapidjson/pointer.h
+++ packages/video_player_avplay/tizen/inc/rapidjson/pointer.h
@@ -1068,7 +1068,8 @@ class GenericPointer {
                        \param source Either a JSON Pointer string, or its URI fragment
                       representation. Not need to be null terminated.                  \param
                       length Length of the                  source string.                  \note
-                      Source cannot be                  JSON String Representation of JSON                  Pointer, e.g. In
+                      Source cannot be                  JSON String Representation of JSON
+                      Pointer, e.g. In
                       "/\u0000", \u0000 will                  not be unescaped.
                    */
 #endif
