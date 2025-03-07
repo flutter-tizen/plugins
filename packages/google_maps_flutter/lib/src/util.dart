4c3782dfb3ec35cf0bbd5aa3ee9e59379041c957
Seungsoo Lee <seungsoo47.lee@samsung.com>
2025-03-07 17:05:02 +0900

    Fix code format

--- packages/google_maps_flutter/lib/src/util.dart
+++ packages/google_maps_flutter/lib/src/util.dart
@@ -138,9 +138,10 @@ class GInfoWindowOptions {
 
   @override
   String toString() {
-    final String pos = position != null
-        ? '{lat:${position?.latitude}, lng:${position?.longitude}}'
-        : 'null';
+    final String pos =
+        position != null
+            ? '{lat:${position?.latitude}, lng:${position?.longitude}}'
+            : 'null';
     return '{content:$content, pixelOffset:null , position:$pos, zIndex:$zIndex}';
   }
 }
@@ -204,9 +205,7 @@ class GInfoWindow {
 /// This class represents a geographical location on the map as a Marker.
 class GMarker {
   /// GMarker Constructor.
-  GMarker([GMarkerOptions? opts])
-      : id = _gid++,
-        _options = opts {
+  GMarker([GMarkerOptions? opts]) : id = _gid++, _options = opts {
     _createMarker(opts);
   }
 
@@ -620,9 +619,11 @@ class GMarkerClusterer {
 
   /// Removes a marker from the [GMarkerClusterer].
   Future<bool> removeMarker(GMarker marker, bool? noDraw) async {
-    final bool result = await webController!.runJavaScriptReturningResult(
-      '${toString()}.removeMarker($marker, $noDraw);',
-    ) as bool;
+    final bool result =
+        await webController!.runJavaScriptReturningResult(
+              '${toString()}.removeMarker($marker, $noDraw);',
+            )
+            as bool;
 
     return result;
   }
