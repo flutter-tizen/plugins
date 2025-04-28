1dc60b52cb3af6960f1ebb5449d54f959b18d9cd
yying.jin <yying.jin@samsung.com>
2025-04-28 15:24:23 +0800

    synchronize isPlaying state

--- packages/video_player_videohole/lib/video_player_platform_interface.dart
+++ packages/video_player_videohole/lib/video_player_platform_interface.dart
@@ -268,6 +268,7 @@ class VideoEvent {
     this.size,
     this.buffered,
     this.text,
+    this.isPlaying,
   });
 
   /// The type of the event.
@@ -292,6 +293,11 @@ class VideoEvent {
   ///
   /// Only used if [eventType] is [VideoEventType.subtitleUpdate].
   final String? text;
+
+  /// Whether the video is currently playing.
+  ///
+  /// Only used if [eventType] is [VideoEventType.isPlayingStateUpdate].
+  final bool? isPlaying;
 
   @override
   bool operator ==(Object other) {
@@ -336,6 +342,12 @@ enum VideoEventType {
 
   /// Updated the video subtitle text.
   subtitleUpdate,
+
+  /// The playback state of the video has changed.
+  ///
+  /// This event is fired when the video starts or pauses due to user actions or
+  /// phone calls, or other app media such as music players.
+  isPlayingStateUpdate,
 
   /// An unknown event has been received.
   unknown,
