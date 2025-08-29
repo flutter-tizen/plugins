d7f74cdc49d226903a568bc83d03c5fea3d47bdb
JunsuChoi <jsuya.choi@samsung.com>
2025-08-29 20:22:34 +0900

    ++

--- packages/video_player_avplay/tizen/prebuild.sh
+++ packages/video_player_avplay/tizen/prebuild.sh
@@ -1,10 +1,12 @@
 BUILD_ARCH=${1}
 LIB_API_VERSION=""
 
-if [ "$API_VERSION" = "6.5" ] || [ "$API_VERSION" = "7.0" ] || [ "$API_VERSION" = "8.0" ] || [ "$API_VERSION" = "9.0" ]; then
+if [ "$API_VERSION" = "6.0" ]; then
+  LIB_API_VERSION="6.0"
+elif [ "$API_VERSION" = "6.5" ] || [ "$API_VERSION" = "7.0" ] || [ "$API_VERSION" = "8.0" ] || [ "$API_VERSION" = "9.0" ]; then
   LIB_API_VERSION="6.5"
 else
-  LIB_API_VERSION=${API_VERSION}
+  LIB_API_VERSION="10.0"
 fi
 
 SRC_DIR="lib/${BUILD_ARCH}/${LIB_API_VERSION}"
