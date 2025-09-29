6e81d5a75e2a1683b33feca1ede3058c8b477a5b
JunsuChoi <jsuya.choi@samsung.com>
2025-09-29 11:32:09 +0900

    [video_player_avplay] Fix type missmatch
    fix to below error on tizen 10.0 rootstrap
    
    src/plus_player.cc:1366:9: error: cannot initialize a variable of type 'char *' with an rvalue of type 'const char *'
      char *data = strstr(ad_data, prefix);
            ^      ~~~~~~~~~~~~~~~~~~~~~~~

--- packages/video_player_avplay/tizen/src/plus_player.cc
+++ packages/video_player_avplay/tizen/src/plus_player.cc
@@ -1359,13 +1359,13 @@ void PlusPlayer::OnStateChangedToPlaying(void *user_data) {
 
 void PlusPlayer::OnADEventFromDash(const char *ad_data, void *user_data) {
   const char *prefix = "AD_INFO: ";
-  char *data = strstr(ad_data, prefix);
+  const char *data = strstr(ad_data, prefix);
   if (!data) {
     LOG_ERROR("[PlusPlayer] Invalid ad_data.");
     return;
   }
   data += strlen(prefix);
-  data[strlen(data) - 1] = '\0';
+  const_cast<char *>(data)[strlen(data) - 1] = '\0';
   LOG_INFO("[PlusPlayer] AD info: %s", data);
 
   rapidjson::Document doc;
