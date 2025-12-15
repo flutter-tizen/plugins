202c243a41cf1b628e96b7b711d2ad73ccf0ece7
JunsuChoi <jsuya.choi@samsung.com>
2025-12-15 15:16:04 +0900

    CI Test

--- packages/audioplayers/tizen/src/audio_player.cc
+++ packages/audioplayers/tizen/src/audio_player.cc
@@ -1,3 +1,5 @@
+//test
+
 // Copyright 2021 Samsung Electronics Co., Ltd. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
