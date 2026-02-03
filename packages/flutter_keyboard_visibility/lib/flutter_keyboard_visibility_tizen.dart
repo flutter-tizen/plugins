9f33b77b6ee7297e62e561e40f6b1713b2f3928f
JunsuChoi <jsuya.choi@samsung.com>
2026-02-03 14:10:34 +0900

    Introduce flutter_keyboard_visibility plugin
    - Add flutter_keyboard_visibility_tizen plugin implementation
    - Implement keyboard visibility detection using Tizen's inputpanel event channel

---
+++ packages/flutter_keyboard_visibility/lib/flutter_keyboard_visibility_tizen.dart
@@ -0,0 +1,5 @@
+// Copyright 2025 Samsung Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+export 'src/flutter_keyboard_visibility_tizen.dart';
