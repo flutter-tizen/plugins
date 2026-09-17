fe48164ede5e04fd18e227296fe497cae6383f27
JunsuChoi <jsuya.choi@samsung.com>
2026-09-17 17:05:19 +0900

    [in_app_purchase] Use wrapper api

--- packages/in_app_purchase/tizen/src/billing_manager.cc
+++ packages/in_app_purchase/tizen/src/billing_manager.cc
@@ -4,7 +4,6 @@
 
 #include "billing_manager.h"
 
-#include <dlfcn.h>
 #include <flutter/encodable_value.h>
 #include <flutter/standard_method_codec.h>
 
@@ -14,26 +13,12 @@
 #include <string>
 #include <variant>
 
-#include "billing_service_proxy.h"
+#include "ftpw_in_app_purchase.h"
 #include "log.h"
 #include "messages.h"
 #include "rapidjson/document.h"
 
 namespace in_app_purchase_tizen {
-
-static std::string ServerTypeToString(billing_server_type server_type) {
-  switch (server_type) {
-    case SERVERTYPE_OPERATE:
-      return "PRD";
-      break;
-    case SERVERTYPE_DEV:
-      return "DEV";
-      break;
-    default:
-      return "NONE";
-      break;
-  }
-}
 
 static flutter::EncodableValue ConvertVariantToEncodable(
     std::variant<int, double> var) {
@@ -45,38 +30,13 @@ static flutter::EncodableValue ConvertVariantToEncodable(
   return flutter::EncodableValue();
 }
 
-bool BillingManager::Init() {
-  LOG_INFO("[BillingManager] Init billing api.");
-
-  if (!BillingWrapper::GetInstance().Initialize()) {
-    LOG_ERROR("[BillingManager] Fail to initialize billing APIs.");
-    return false;
-  }
-  return true;
-}
-
 std::optional<std::string> BillingManager::GetCustomId() {
-  void *handle = dlopen("libsso_api.so", RTLD_LAZY);
-  if (!handle) {
-    LOG_ERROR("[BillingManager] Fail to open sso APIs.");
-    return std::nullopt;
-  }
-  FuncSsoGetLoginInfo sso_get_login_info =
-      reinterpret_cast<FuncSsoGetLoginInfo>(
-          dlsym(handle, "sso_get_login_info"));
-  if (!sso_get_login_info) {
-    LOG_ERROR("[BillingManager] Fail to find the sso_get_login_info symbol.");
-    dlclose(handle);
-    return std::nullopt;
-  }
-
   std::optional<std::string> custom_id;
   sso_login_info_s login_info = {};
-  int ret = sso_get_login_info(&login_info);
-  if (ret == SSO_SUCCESS) {
+  if (!ftpw_in_app_purchase_sso_get_login_info(&login_info)) {
     custom_id = login_info.uid;
   } else {
-    LOG_ERROR("[BillingManager] Fail to get the login info. (%d)", ret);
+    LOG_ERROR("[BillingManager] Fail to get the login info.");
   }
   // NOTE: written through volatile because a plain memset on a struct that
   // is dead afterwards is dropped by the optimizer.
@@ -84,72 +44,23 @@ std::optional<std::string> BillingManager::GetCustomId() {
   for (std::size_t i = 0; i < sizeof(login_info); ++i) {
     bytes[i] = 0;
   }
-  dlclose(handle);
   return custom_id;
 }
 
 std::optional<std::string> BillingManager::GetCountryCode() {
-  void *handle = dlopen("libvconf.so.0", RTLD_LAZY);
-  if (!handle) {
-    LOG_ERROR("[BillingManager] Fail to open vconf APIs.");
-    return std::nullopt;
-  }
-  FuncVconfGetStr vconf_get_str =
-      reinterpret_cast<FuncVconfGetStr>(dlsym(handle, "vconf_get_str"));
-  if (!vconf_get_str) {
-    LOG_ERROR("[BillingManager] Fail to find the vconf_get_str symbol.");
-    dlclose(handle);
-    return std::nullopt;
-  }
-
   std::optional<std::string> country_code;
-  char *value = vconf_get_str("db/comss/countrycode");
+  char *value = ftpw_in_app_purchase_get_county_code();
   if (value) {
     country_code = value;
     free(value);
   } else {
-    LOG_ERROR("[BillingManager] Fail to read db/comss/countrycode.");
+    LOG_ERROR("[BillingManager] Fail to get country code.");
   }
-  dlclose(handle);
   return country_code;
 }
 
 bool BillingManager::IsAvailable(FunctionResult<bool> result) {
   LOG_INFO("[BillingManager] Check billing server is available.");
-
-  void *handle = dlopen("libcapi-system-info.so.0", RTLD_LAZY);
-  if (!handle) {
-    LOG_ERROR("[BillingManager] Fail to open system APIs.");
-  } else {
-    FuncSystemInfGetValueInt system_info_get_value_int =
-        reinterpret_cast<FuncSystemInfGetValueInt>(
-            dlsym(handle, "system_info_get_value_int"));
-    if (system_info_get_value_int) {
-      int tv_server_type = 0;
-      int ret = system_info_get_value_int(SYSTEM_INFO_KEY_INFO_LINK_SERVER_TYPE,
-                                          &tv_server_type);
-      if (ret == SYSTEM_INFO_ERROR_NONE) {
-        switch (tv_server_type) {
-          case PRD:
-            billing_server_type_ = SERVERTYPE_OPERATE;
-            break;
-          case DEV:
-            billing_server_type_ = SERVERTYPE_DEV;
-            break;
-          default:
-            billing_server_type_ = SERVERTYPE_NONE;
-            break;
-        }
-        LOG_INFO("[BillingManager] Billing Server Type is %d",
-                 billing_server_type_);
-      } else {
-        LOG_ERROR("[BillingManager] Fail to get TV server type.");
-        dlclose(handle);
-        return false;
-      }
-    }
-    dlclose(handle);
-  }
 
   if (is_available_callback_) {
     LOG_ERROR("[BillingManager] IsAvailable is already called.");
@@ -157,8 +68,8 @@ bool BillingManager::IsAvailable(FunctionResult<bool> result) {
   }
 
   is_available_callback_ = std::move(result);
-  bool ret = BillingWrapper::GetInstance().service_billing_is_service_available(
-      billing_server_type_, OnAvailable, this);
+  bool ret = ftpw_in_app_purchase_service_billing_is_service_available(
+      OnAvailable, this);
   if (!ret) {
     is_available_callback_ = nullptr;
     LOG_ERROR("[BillingManager] service_billing_is_service_available failed.");
@@ -179,9 +90,9 @@ bool BillingManager::GetProductList(
   }
 
   get_product_list_callback_ = std::move(result);
-  bool ret = BillingWrapper::GetInstance().service_billing_get_products_list(
-      app_id, country_code, page_size, page_number, check_value,
-      billing_server_type_, OnProducts, this);
+  bool ret = ftpw_in_app_purchase_service_billing_get_products_list(
+      app_id, country_code, page_size, page_number, check_value, OnProducts,
+      this);
   if (!ret) {
     get_product_list_callback_ = nullptr;
     LOG_ERROR("[BillingManager] service_billing_get_products_list failed.");
@@ -202,9 +113,9 @@ bool BillingManager::GetPurchaseList(
   }
 
   get_purchase_list_callback_ = std::move(result);
-  bool ret = BillingWrapper::GetInstance().service_billing_get_purchase_list(
-      app_id, custom_id, country_code, page_number, check_value,
-      billing_server_type_, OnPurchase, this);
+  bool ret = ftpw_in_app_purchase_service_billing_get_purchase_list(
+      app_id, custom_id, country_code, page_number, check_value, OnPurchase,
+      this);
   if (!ret) {
     get_purchase_list_callback_ = nullptr;
     LOG_ERROR("[BillingManager] service_billing_get_purchase_list failed.");
@@ -224,9 +135,8 @@ bool BillingManager::BuyItem(const char *app_id, const char *detail_info,
   }
 
   buy_item_callback_ = std::move(result);
-  bool ret = BillingWrapper::GetInstance().service_billing_buyitem(
-      app_id, ServerTypeToString(billing_server_type_).c_str(), detail_info);
-  BillingWrapper::GetInstance().service_billing_set_buyitem_cb(OnBuyItem, this);
+  bool ret = ftpw_in_app_purchase_service_billing_buyitem(app_id, detail_info);
+  ftpw_in_app_purchase_service_billing_set_buyitem_cb(OnBuyItem, this);
   if (!ret) {
     buy_item_callback_ = nullptr;
     LOG_ERROR("[BillingManager] service_billing_buyitem failed.");
@@ -246,9 +156,8 @@ bool BillingManager::VerifyInvoice(
   }
 
   verify_invoice_callback_ = std::move(result);
-  bool ret = BillingWrapper::GetInstance().service_billing_verify_invoice(
-      app_id, custom_id, invoice_id, country_code, billing_server_type_,
-      OnVerify, this);
+  bool ret = ftpw_in_app_purchase_service_billing_verify_invoice(
+      app_id, custom_id, invoice_id, country_code, OnVerify, this);
   if (!ret) {
     verify_invoice_callback_ = nullptr;
     LOG_ERROR("[BillingManager] service_billing_verify_invoice failed.");
@@ -528,10 +437,6 @@ void BillingManager::OnVerify(const char *detail_result, void *user_data) {
         FlutterError("Invalid argument", "verify_invoice_callback_ is null !"));
   }
   self->verify_invoice_callback_ = nullptr;
-}
-
-void BillingManager::Dispose() {
-  LOG_INFO("[BillingManager] Dispose billing.");
 }
 
 }  // namespace in_app_purchase_tizen
