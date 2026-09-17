4e0973c6a0e160442f1b809005088598e170c38a
JunsuChoi <jsuya.choi@samsung.com>
2026-09-17 17:05:19 +0900

    [in_app_purchase] Use wrapper api

--- packages/in_app_purchase/tizen/src/billing_manager.h
+++ packages/in_app_purchase/tizen/src/billing_manager.h
@@ -5,8 +5,6 @@
 #ifndef FLUTTER_PLUGIN_BILLING_MANAGER_H
 #define FLUTTER_PLUGIN_BILLING_MANAGER_H
 
-#include <tizen_error.h>
-
 #include <cassert>
 #include <iostream>
 #include <mutex>
@@ -14,50 +12,10 @@
 #include <string>
 #include <variant>
 
-#include "billing_service_proxy.h"
 #include "messages.h"
 #include "rapidjson/document.h"
 
-#define SSO_API_MAX_STRING_LEN 128
-#define SSO_SUCCESS 0
-
 namespace in_app_purchase_tizen {
-
-typedef struct sso_login_info {
-  char login_id[SSO_API_MAX_STRING_LEN];
-  char login_pwd[SSO_API_MAX_STRING_LEN];
-  char login_guid[SSO_API_MAX_STRING_LEN];
-  char uid[SSO_API_MAX_STRING_LEN];
-  char user_icon[SSO_API_MAX_STRING_LEN * 8];
-} sso_login_info_s;
-
-typedef enum {
-  PRD = 0,
-  DEV,
-} tv_server_type;
-
-typedef enum {
-  SYSTEM_INFO_ERROR_NONE = TIZEN_ERROR_NONE, /**< Successful */
-  SYSTEM_INFO_ERROR_INVALID_PARAMETER =
-      TIZEN_ERROR_INVALID_PARAMETER, /**< Invalid parameter */
-  SYSTEM_INFO_ERROR_OUT_OF_MEMORY =
-      TIZEN_ERROR_OUT_OF_MEMORY, /**< Out of memory */
-  SYSTEM_INFO_ERROR_IO_ERROR =
-      TIZEN_ERROR_IO_ERROR, /**< An input/output error occurred when reading
-                               value from system */
-  SYSTEM_INFO_ERROR_PERMISSION_DENIED =
-      TIZEN_ERROR_PERMISSION_DENIED, /**< No permission to use the API */
-  SYSTEM_INFO_ERROR_NOT_SUPPORTED =
-      TIZEN_ERROR_NOT_SUPPORTED, /**< Not supported parameter (Since 3.0) */
-} system_info_error_e;
-
-typedef enum {
-  SYSTEM_INFO_KEY_INFO_LINK_SERVER_TYPE = 126,
-} system_info_key_e;
-
-typedef int (*FuncSsoGetLoginInfo)(sso_login_info_s *login_info);
-typedef char *(*FuncVconfGetStr)(const char *in_key);
-typedef int (*FuncSystemInfGetValueInt)(system_info_key_e key, int *value);
 
 template <typename T>
 using FunctionResult = std::function<void(ErrorOr<T>)>;
@@ -102,8 +60,6 @@ class BillingManager {
   explicit BillingManager() {}
   ~BillingManager(){};
 
-  bool Init();
-  void Dispose();
   bool IsAvailable(FunctionResult<bool> result);
   bool BuyItem(const char *app_id, const char *detail_info,
                FunctionResult<BillingBuyData> result);
@@ -127,8 +83,6 @@ class BillingManager {
                         void *user_data);
   static void OnAvailable(const char *detail_result, void *user_data);
   static void OnVerify(const char *detail_result, void *user_data);
-
-  billing_server_type billing_server_type_;
 
   FunctionResult<bool> is_available_callback_;
   FunctionResult<BillingBuyData> buy_item_callback_;
