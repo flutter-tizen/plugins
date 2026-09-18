f89d139f5b01075046d08526544d80b8b4584741
JunsuChoi <jsuya.choi@samsung.com>
2026-09-18 17:27:53 +0900

    [tizen_app_manager] Use wrapper API

---
+++ packages/tizen_app_manager/tizen/inc/ftpw_tizen_app_manager.h
@@ -0,0 +1,17 @@
+// Copyright 2026 Samsung Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef FLUTTER_TIZEN_PLUGINS_WRAPPER_TIZEN_APP_MANAGER_H_
+#define FLUTTER_TIZEN_PLUGINS_WRAPPER_TIZEN_APP_MANAGER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int ftpw_tizen_app_manager_app_manager_terminate_app(void *context);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
